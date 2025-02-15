97ba005431ec2fc98af6e2e58f973d277c3b004a
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-15 23:27:24 +0530

    [Naman] Add. Solution of Testing Banking Transaction

---
+++ Day5/src/main/java/junit/advancedjunit/testingbanktransaction/BankAccount.java
@@ -0,0 +1,30 @@
+package junit.advancedjunit.testingbanktransaction;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be positive");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
