bb6877da8af0359fd5c62fd4973cce27bfd961ab
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-15 23:28:03 +0530

    [Naman] Add. Solution of testing password strength

---
+++ Day5/src/main/java/junit/advancedjunit/testingpasswordstrength/PasswordValidator.java
@@ -0,0 +1,23 @@
+package junit.advancedjunit.testingpasswordstrength;
+
+public class PasswordValidator {
+
+    // Method to validate password strength
+    public static boolean isValid(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        boolean hasUpperCase = false;
+        boolean hasDigit = false;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUpperCase = true;
+            }
+            if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+        }
+        return hasUpperCase && hasDigit;
+    }
+}
