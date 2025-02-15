5f387bd56702681a8600158dd508a4fc5a24b0d1
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-15 23:50:17 +0530

    [Naman] Add. Solution of testing exception handling

---
+++ Day5/src/main/java/junit/basicjunittestproblems/testingexceptionhandling/Calculator.java
@@ -0,0 +1,10 @@
+package junit.basicjunittestproblems.testingexceptionhandling;
+
+public class Calculator {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
