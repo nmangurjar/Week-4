3958bd28557c8bada6e9a088c6c4f5fcc74d5954
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-15 20:42:02 +0530

    [Naman] Add. Solution of  Find Repeating Words in a Sentence

---
+++ Day5/src/main/java/regex/advancedproblems/repeatingwords/RepeatingWords.java
@@ -0,0 +1,18 @@
+package regex.advancedproblems.repeatingwords;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RepeatingWords {
+    public static void main(String[] args) {
+        String text = "This is is a repeated repeated word test.";
+        String regex = "\\b(\\w+)\\b(?:\\s+\\b\\1\\b)+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()){
+            System.out.println(matcher.group());
+        }
+
+    }
+}
