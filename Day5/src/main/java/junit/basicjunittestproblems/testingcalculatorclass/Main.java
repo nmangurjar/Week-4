3a915473c9f24a07604c101ce62d630ad8e19897
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-02-15 20:45:25 +0530

    [Naman] Add. Solution of  Testing a Calculator Class

---
+++ Day5/src/main/java/junit/basicjunittestproblems/testingcalculatorclass/Main.java
@@ -0,0 +1,14 @@
+package junit.basicjunittestproblems.testingcalculatorclass;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+
+        System.out.println("Addition Result :"+  c.add(10,2));
+        System.out.println("Addition Result :"+  c.subtract(10,2));
+        System.out.println("Addition Result :"+  c.multiply(10,2));
+        System.out.println("Addition Result :"+  c.divide(10,2));
+
+    }
+}
